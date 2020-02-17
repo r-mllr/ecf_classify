ee43e1fe1acdf5bd41fbed2182614181e501f89d
Raphael Müller <raphael.mueller@computational.bio.uni-giessen.de>
2020-02-17 16:06:00 +0000

    hmmer module

---
+++ lib/ecf_classify/hmmer.rb
@@ -0,0 +1,28 @@
+module EcfClassify
+  module HMMER
+    DBLOCATION = {
+      :general => "data/HMM_general/all.hmm",
+      :sigma3 => "data/HMM_s3/Sigma70_r3.hmm",
+      :pfam => "data/Pfam_domains/sigma2and4.hmm",
+      :groups => "data/HMM_groups/all_models.hmm",
+      :subgroups => "data/HMM_subgroups/all_models.hmm",
+    }
+    def self.hmmscan(infile, outfile, db)
+      if DBLOCATION.key? db
+        hmm = Utils.path(DBLOCATION[db])
+        if File.exists? hmm+".gz"
+          num = File.read(infile).scan(%r{^>}).count
+          subout = IO.popen("mkfifo #{hmm}; for i in {1..num}; do gunzip -c #{hmm}.gz > #{hmm}; done; rm #{hmm}")
+        else
+          subout = IO.popen("ls")
+        end
+        cmd = "hmmscan --noali --domtblout #{outfile} #{Utils.path(DBLOCATION[db])} #{infile} 2>&1"
+        out = `#{cmd}`
+        subout.close
+        return [out,$?.exitstatus]
+      else
+        return ["No such database",2]
+      end
+    end
+  end
+end
